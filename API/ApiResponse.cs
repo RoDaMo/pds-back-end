a593bc9813b88b47c68517fa846a821abcb735ad
Caioferrari04 <caioferrari0484@gmail.com>
2023-03-11 16:09:56 -0300

    Initial config

---
+++ API/ApiResponse.cs
@@ -0,0 +1,23 @@
+﻿namespace pds_back_end.API;
+
+public class ApiResponse<T>
+{
+    public ApiResponse()
+    {
+        Message = string.Empty;
+        Succeed = false;
+    }
+
+    public ApiResponse(string message, bool succeed, T results)
+    {
+        Message = message;
+        Succeed = succeed;
+        Results = results;
+    }
+
+
+    public string Message { get; set; }
+    public bool Succeed { get; set; }
+    public T? Results { get; set; }
+}
+
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using pds_back_end.API;
+
+namespace pds_back_end.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HomeController : ControllerBase
+{
+    [HttpGet(Name = "Test")]
+    public ApiResponse<string> TestResponse() =>
+        new() { Succeed = true, Message = "Bem vindo à API!", Results = "Resultados" };
+}
+
