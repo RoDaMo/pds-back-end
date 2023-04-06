117200fa732d4173300c8ef700854be174cccd77
AlexsandroCampos <alexsandro.campos@aluno.ifsp.edu.br>
2023-04-06 08:42:25 -0300

    feat: add validations to create championship

--- Services/ChampionshipService.cs
+++ Services/ChampionshipService.cs
@@ -1,3 +1,4 @@
+using pds_back_end.Controllers.Validations;
 using pds_back_end.Models;
 
 namespace pds_back_end.Services;
@@ -10,15 +11,44 @@ public class ChampionshipService
     {
         _dbService = dbService;
     }
-    public void CreateValidationAsync(Championship championship)
+    public async Task<List<string>> CreateValidationAsync(Championship championship)
     {
-        CreateSendAsync(championship);
+        var errorMessages = new List<string>();
+
+        var championshipValidator = new ChampionshipValidator();
+    
+        var result = championshipValidator.Validate(championship);
+
+        if (!result.IsValid)
+        {
+            errorMessages = result.Errors.Select(x => x.ErrorMessage).ToList();
+            return errorMessages;
+        }
+
+        var result2 = await CreateSendAsync(championship);
+        if(!result2)
+        {
+            errorMessages.Add("Erro ao cadastrar campeonato");
+            return errorMessages;
+        }
+
+        return errorMessages;
     }
 
-    public async void CreateSendAsync(Championship championship)
+    public async Task<bool> CreateSendAsync(Championship championship)
     {
-        var result = await _dbService.EditData(
+        try
+        {
+            var result = await _dbService.EditData(
                 "INSERT INTO championships (id, name, prize, sportsid, initialdate, finaldate) VALUES (@Id, @Name, @Prize, @SportsId, @Initialdate, @Finaldate)",
                 championship);
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return false;
+        }
+        
     }
 }
