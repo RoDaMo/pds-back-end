3d32a729196711bd6827d1ee6780e1e18db8368f
AlexsandroCampos <alexsandro.campos@aluno.ifsp.edu.br>
2023-04-06 08:43:14 -0300

    feat: add championship validator

---
+++ Validations/ChampionshipValidator.cs
@@ -0,0 +1,38 @@
+using FluentValidation;
+using pds_back_end.Models;
+
+namespace pds_back_end.Controllers.Validations;
+
+public class ChampionshipValidator : AbstractValidator<Championship>
+{
+    public ChampionshipValidator()
+    {
+        RuleFor(c => c.Name)
+            .NotEmpty()
+            .WithMessage("Campo Nome não pode ser vazio.");
+         RuleFor(c => c.Name)
+            .Length(2, 20)
+            .WithMessage("Campo Nome deve ter entre 4 e 50 caracteres.");
+        
+        RuleFor(c => c.Prize)
+            .NotEmpty()
+            .WithMessage("Campo Prêmio não pode ser vazio.");
+         RuleFor(c => c.Prize)
+            .Length(2, 20)
+            .WithMessage("Campo Prêmio deve ter entre 3 e 20 caracteres.");
+
+        RuleFor(c => c.InitialDate)
+            .NotEmpty()
+            .WithMessage("Campo Data Inicial não pode ser vazio.");
+        RuleFor(c => c.InitialDate)
+            .GreaterThanOrEqualTo(c => DateTime.UtcNow.Date)
+            .WithMessage("Campo Data Inicial não pode ser anterior a hoje.");
+        RuleFor(c => c.InitialDate)
+            .LessThanOrEqualTo(c => c.FinalDate)
+            .WithMessage("Campo Data Inicial não pode ser posterior à data final.");
+
+         RuleFor(c => c.FinalDate)
+            .NotEmpty()
+            .WithMessage("Campo Data Final não pode ser vazio.");
+    }
+}
