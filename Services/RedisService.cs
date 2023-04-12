0dfa1f2b89b922f96f648091226012397f9f8cd3
Caioferrari04 <caioferrari0484@gmail.com>
2023-04-12 19:21:48 -0300

    fix: fixing redis error and namespace naming mistake

--- Services/RedisService.cs
+++ Services/RedisService.cs
@@ -1,19 +1,20 @@
-using StackExchange.Redis;
+using ServiceStack.Redis;
 
-namespace pds_back_end.Services;
+namespace PlayOffsApi.Services;
 
 public class RedisService
 {
-  private readonly IConnectionMultiplexer _redis;
+  private readonly RedisManagerPool _redis;
   public RedisService(IWebHostEnvironment environment)
   {
-    string url = "localhost:6379";
+    string url = "redis://default:subkZye2ZCf1dm1cC2w8@containers-us-west-200.railway.app:6689";
 
     if (environment.IsProduction()) 
       url = Environment.GetEnvironmentVariable("REDIS_URL");
 
-    _redis = ConnectionMultiplexer.Connect(url);
+    // _redis = ConnectionMultiplexer.Connect(url);
+    _redis = new RedisManagerPool(url);
   }
 
-  public IDatabase Database { get => _redis.GetDatabase(); }
+  public async Task<IRedisClientAsync> GetDatabase() => await _redis.GetClientAsync();
 }
