23848807c8f3bacdfe810853e9864084030bb3f1
Caioferrari04 <caioferrari0484@gmail.com>
2023-04-12 19:26:30 -0300

    fix: leaked. again.

--- Services/RedisService.cs
+++ Services/RedisService.cs
@@ -7,12 +7,11 @@ public class RedisService
   private readonly RedisManagerPool _redis;
   public RedisService(IWebHostEnvironment environment)
   {
-    string url = "redis://default:subkZye2ZCf1dm1cC2w8@containers-us-west-200.railway.app:6689";
+    string url = "localhost:6379";
 
     if (environment.IsProduction()) 
       url = Environment.GetEnvironmentVariable("REDIS_URL");
 
-    // _redis = ConnectionMultiplexer.Connect(url);
     _redis = new RedisManagerPool(url);
   }
 
