adaacdee1fefb99afca03a911b9fc731c48d1ba7
Caioferrari04 <caioferrari0484@gmail.com>
2023-04-15 23:08:08 -0300

    feat: internacionalization completed

--- Program.cs
+++ Program.cs
@@ -1,20 +1,42 @@
+using Microsoft.AspNetCore.Localization;
 using PlayOffsApi.Services;
+using ServiceStack;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+
 builder.Services.AddScoped<DbService>();
 builder.Services.AddScoped<ChampionshipService>();
 builder.Services.AddSingleton<RedisService>();
 builder.Services.AddSingleton<ElasticService>();
 builder.Services.AddScoped<SportService>();
 
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    var supportedCultures = new[] { new CultureInfo("en"), new CultureInfo("pt-BR") };
+
+    options.DefaultRequestCulture = new RequestCulture("pt-BR");
+    options.SupportedCultures = supportedCultures;
+    options.SupportedUICultures = supportedCultures;
+    options.AddInitialRequestCultureProvider(new CustomRequestCultureProvider(async context =>
+    {
+        var acceptLanguageHeader = context.Request.Headers["Accept-Language"].ToString();
+        var culture = GetTrueLanguage(acceptLanguageHeader);
+        return new ProviderCultureResult(culture);
+    }));
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddCors(options => {
-    options.AddDefaultPolicy(policy => {
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
         policy.AllowAnyOrigin();
         policy.AllowAnyHeader();
         policy.AllowAnyMethod();
@@ -39,7 +61,14 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
+app.UseRequestLocalization(app.Services.GetRequiredService<Microsoft.Extensions.Options.IOptions<RequestLocalizationOptions>>().Value);
 app.MapControllers();
 app.UseCors();
 app.Run();
+
+static string GetTrueLanguage(string falseLanguage) => falseLanguage switch
+{
+    "ptbr" => "pt-BR",
+    "en" => "en",
+    _ => "pt-BR",
+};
--- Services/ElasticService.cs
+++ Services/ElasticService.cs
@@ -1,39 +1,50 @@
 using Elastic.Clients.Elasticsearch;
 using Elastic.Transport;
+using Resource = PlayOffsApi.Resources.Generic;
 
 namespace PlayOffsApi.Services;
 
-public class ElasticService 
+public class ElasticService
 {
-  public readonly ElasticsearchClient _client;
-  public ElasticService(IConfiguration configuration, IWebHostEnvironment environment)
-  {
-    ElasticsearchClientSettings settings;
-    if (environment.IsProduction()) 
+    public readonly ElasticsearchClient _client;
+    public ElasticService(IConfiguration configuration, IWebHostEnvironment environment)
     {
-      var CLOUD_ID = Environment.GetEnvironmentVariable("CLOUD_ID");
-      var API_KEY = Environment.GetEnvironmentVariable("ELASTIC_API_KEY");
-      var ELASTIC_USER = Environment.GetEnvironmentVariable("ELASTIC_USER");
-      var ELASTIC_PASSWORD = Environment.GetEnvironmentVariable("ELASTIC_PASSWORD");
+        ElasticsearchClientSettings settings;
+        if (environment.IsProduction())
+        {
+            var CLOUD_ID = Environment.GetEnvironmentVariable("CLOUD_ID");
+            var API_KEY = Environment.GetEnvironmentVariable("ELASTIC_API_KEY");
+            var ELASTIC_USER = Environment.GetEnvironmentVariable("ELASTIC_USER");
+            var ELASTIC_PASSWORD = Environment.GetEnvironmentVariable("ELASTIC_PASSWORD");
 
-      settings = new ElasticsearchClientSettings(CLOUD_ID, new ApiKey(API_KEY))
-        .Authentication(new BasicAuthentication(ELASTIC_USER, ELASTIC_PASSWORD));
+            settings = new ElasticsearchClientSettings(CLOUD_ID, new ApiKey(API_KEY))
+              .Authentication(new BasicAuthentication(ELASTIC_USER, ELASTIC_PASSWORD));
+        }
+        else
+        {
+            settings = new ElasticsearchClientSettings(new Uri(configuration.GetValue<string>("ElasticURI")))
+              .CertificateFingerprint(configuration.GetValue<string>("Fingerprint"))
+              .Authentication(new BasicAuthentication("elastic", configuration.GetValue<string>("Password")))
+              .EnableDebugMode()
+              .PrettyJson();
+        }
+
+        _client = new ElasticsearchClient(settings);
     }
-    else 
+
+    public async Task<string> GetClusterHealth()
     {
-      settings = new ElasticsearchClientSettings(new Uri(configuration.GetValue<string>("ElasticURI")))
-        .CertificateFingerprint(configuration.GetValue<string>("Fingerprint"))
-        .Authentication(new BasicAuthentication("elastic", configuration.GetValue<string>("Password")))
-        .EnableDebugMode()
-        .PrettyJson();
+        var resultado = await _client.Cluster.HealthAsync();
+        return resultado.IsSuccess().ToString();
     }
 
-    _client = new ElasticsearchClient(settings);
-  }
+    public async Task<List<T>> SearchAsync<T>(Action<SearchRequestDescriptor<T>> request)
+    {
+        var resposta = await _client.SearchAsync(request);
 
-  public async Task<string> GetClusterHealth() 
-  {
-    var resultado = await _client.Cluster.HealthAsync();
-    return resultado.IsSuccess().ToString();
-  } 
+        if (!resposta.IsValidResponse)
+            throw new ApplicationException(Resource.GenericErrorMessage);
+
+        return resposta.Documents.ToList();
+    }
 }
