ca6698f77f8fffb7ae036b0c51a9f4084237a019
AlexsandroCampos <alexsandro.campos@aluno.ifsp.edu.br>
2023-04-23 23:13:42 -0300

    feat: add team service

---
+++ Services/TeamService.cs
@@ -0,0 +1,49 @@
+using PlayOffsApi.Models;
+using Resource = PlayOffsApi.Resources.Generic;
+
+namespace PlayOffsApi.Services;
+
+public class TeamService
+{
+    private readonly DbService _dbService;
+    private readonly ElasticService _elasticService;
+	private const string INDEX = "teams";
+
+
+    public TeamService(DbService dbService, ElasticService elasticService)
+	{
+		_dbService = dbService;
+        _elasticService = elasticService;
+	}
+
+    public async Task<List<string>> CreateValidationAsync(Team team)
+	{
+		var errorMessages = new List<string>();
+
+		// var teamValidator = new TeamValidator();
+
+		// var result = teamValidator.Validate(team);
+
+		// if (!result.IsValid)
+		// {
+		// 	errorMessages = result.Errors.Select(x => x.ErrorMessage).ToList();
+		// 	return errorMessages;
+		// }
+
+		await CreateSendAsync(team);
+
+		return errorMessages;
+	}
+
+	public async Task CreateSendAsync(Team team)
+	{
+		team.Id = await _dbService.EditData(
+			"INSERT INTO teams (emblem, uniformHome, uniformWay, deleted, sportsid) VALUES (@Emblem, @UniformHome, @UniformWay, @Deleted, @SportsId) RETURNING Id;",
+			team);
+
+		// var resultado = await _elasticService._client.IndexAsync(team, INDEX);
+
+		// if (!resultado.IsValidResponse)
+		// 	throw new ApplicationException(Resource.GenericErrorMessage);
+	}
+}
