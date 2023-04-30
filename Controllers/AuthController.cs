f5ced589c92295fac152f978275f8b459d4900b2
Caioferrari04 <caioferrari0484@gmail.com>
2023-04-30 15:02:33 -0300

    feat: adding jwt authentication

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlayOffsApi.API;
+using PlayOffsApi.Models;
+using PlayOffsApi.Services;
+
+namespace PlayOffsApi.Controllers;
+
+[Route("/auth")]
+[ApiController]
+public class AuthController : ApiBaseController
+{
+	private readonly AuthService _authService;
+	public AuthController(AuthService authService)
+	{
+		_authService = authService;
+	}
+
+	[HttpPost]
+	public async Task<IActionResult> GenerateToken(User user)
+	{
+		try
+		{
+			var token = await Task.Run(() => _authService.GenerateJwtToken(user.Id, user.Email));
+			return ApiOk<string>(token);
+		}
+		catch (ApplicationException ex)
+		{
+			return ApiBadRequest(ex, "Erro");
+		}
+	}
+}
