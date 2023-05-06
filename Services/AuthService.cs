6501b1e42fb996c1ce5590cc9ec2121d4d0b970f
Caioferrari04 <caioferrari0484@gmail.com>
2023-05-06 20:36:09 -0300

    fix: removing email encryption

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -13,14 +13,14 @@ public class AuthService
 	private readonly string _issuer;
 	private readonly string _audience;
 	private readonly DbService _dbService;
-	private readonly byte[] _criptKey;
-	public AuthService(string secretKey, string issuer, string audience, DbService dbService, byte[] criptKey)
+	// private readonly byte[] _criptKey;
+	public AuthService(string secretKey, string issuer, string audience, DbService dbService) // , byte[] criptKey
 	{
 		_secretKey = secretKey;
 		_issuer = issuer;
 		_audience = audience;
 		_dbService = dbService;
-		_criptKey = criptKey;
+		// _criptKey = criptKey;
 	}
 
 	public string GenerateJwtToken(Guid userId, string username)
@@ -57,48 +57,49 @@ public class AuthService
 	public async Task RegisterUser(User newUser)
 	{
 		newUser.PasswordHash = EncryptPassword(newUser.Password);
-		newUser.EmailHash = EncryptEmail(newUser.Email);
+		// TODO: remove
+		newUser.EmailHash = newUser.Email;
 
 		await _dbService.EditData("INSERT INTO users (Name, Username, PasswordHash, EmailHash, Deleted, Birthday) VALUES (@Name, @Username, @PasswordHash, @EmailHash, @Deleted, @Birthday)", newUser);
 	}
 
-	private string EncryptEmail(string plainText)
-	{
-		using Aes aes = Aes.Create();
-		aes.Key = _criptKey;
-		aes.GenerateIV();
+	// private string EncryptEmail(string plainText)
+	// {
+	// 	using Aes aes = Aes.Create();
+	// 	aes.Key = _criptKey;
+	// 	aes.GenerateIV();
 
-		using MemoryStream memoryStream = new MemoryStream();
-		// Write the IV to the beginning of the MemoryStream
-		memoryStream.Write(aes.IV, 0, aes.IV.Length);
+	// 	using MemoryStream memoryStream = new MemoryStream();
+	// 	// Write the IV to the beginning of the MemoryStream
+	// 	memoryStream.Write(aes.IV, 0, aes.IV.Length);
 
-		using CryptoStream cryptoStream = new CryptoStream(memoryStream, aes.CreateEncryptor(), CryptoStreamMode.Write);
-		using StreamWriter streamWriter = new StreamWriter(cryptoStream);
+	// 	using CryptoStream cryptoStream = new CryptoStream(memoryStream, aes.CreateEncryptor(), CryptoStreamMode.Write);
+	// 	using StreamWriter streamWriter = new StreamWriter(cryptoStream);
 
-		streamWriter.Write(plainText);
-		streamWriter.Flush();
-		cryptoStream.FlushFinalBlock();
+	// 	streamWriter.Write(plainText);
+	// 	streamWriter.Flush();
+	// 	cryptoStream.FlushFinalBlock();
 
-		return Convert.ToBase64String(memoryStream.ToArray());
-	}
+	// 	return Convert.ToBase64String(memoryStream.ToArray());
+	// }
 
-	private string DecryptEmail(string cipherText)
-	{
-		byte[] cipherData = Convert.FromBase64String(cipherText);
+	// private string DecryptEmail(string cipherText)
+	// {
+	// 	byte[] cipherData = Convert.FromBase64String(cipherText);
 
-		using Aes aes = Aes.Create();
-		aes.Key = _criptKey;
+	// 	using Aes aes = Aes.Create();
+	// 	aes.Key = _criptKey;
 
-		using MemoryStream memoryStream = new MemoryStream(cipherData);
-		byte[] iv = new byte[aes.IV.Length];
-		memoryStream.Read(iv, 0, iv.Length);
-		aes.IV = iv;
+	// 	using MemoryStream memoryStream = new MemoryStream(cipherData);
+	// 	byte[] iv = new byte[aes.IV.Length];
+	// 	memoryStream.Read(iv, 0, iv.Length);
+	// 	aes.IV = iv;
 
-		using CryptoStream cryptoStream = new CryptoStream(memoryStream, aes.CreateDecryptor(), CryptoStreamMode.Read);
-		using StreamReader streamReader = new StreamReader(cryptoStream);
+	// 	using CryptoStream cryptoStream = new CryptoStream(memoryStream, aes.CreateDecryptor(), CryptoStreamMode.Read);
+	// 	using StreamReader streamReader = new StreamReader(cryptoStream);
 
-		return streamReader.ReadToEnd();
-	}
+	// 	return streamReader.ReadToEnd();
+	// }
 
 	public async Task<User> VerifyCredentials(User user)
 	{
