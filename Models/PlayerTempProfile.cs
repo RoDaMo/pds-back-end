9485940a71eb2f855f41d8be69346c1bcf96e28d
AlexsandroCampos <alexsandro.campos@aluno.ifsp.edu.br>
2023-05-06 23:45:34 -0300

    feat: add player temp profile model

---
+++ Models/PlayerTempProfile.cs
@@ -0,0 +1,30 @@
+namespace PlayOffsApi.Models;
+
+public class PlayerTempProfile
+{
+	public Guid Id { get; set; }
+    public string Name { get; set; }
+	public string ArtisticName { get; set; }
+    public int Number { get; set; }
+    public string Email { get; set; }
+	public int TeamsId { get; set; }
+
+	public PlayerTempProfile(string name, string artisticName, int number, string email)
+	{
+		Name = name;
+		ArtisticName = artisticName;
+		Number = number;
+		Email = email;
+	}
+
+	public PlayerTempProfile(string name, string artisticName, int number, string email, int teamsId)
+	{
+		Name = name;
+		ArtisticName = artisticName;
+		Number = number;
+		Email = email;
+		TeamsId = teamsId;
+	}
+
+	public PlayerTempProfile() { }
+}
