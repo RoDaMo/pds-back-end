b99475225d89594fe5b143d79de02fface3e6025
AlexsandroCampos <alexsandro.campos@aluno.ifsp.edu.br>
2023-05-13 00:52:44 -0300

    feat: add is captain colum and others validations

--- Models/PlayerTempProfile.cs
+++ Models/PlayerTempProfile.cs
@@ -8,22 +8,25 @@ public class PlayerTempProfile
     public int Number { get; set; }
     public string Email { get; set; }
 	public int TeamsId { get; set; }
+	public bool IsCaptain { get; set; }
 
-	public PlayerTempProfile(string name, string artisticName, int number, string email)
+	public PlayerTempProfile(string name, string artisticName, int number, string email, bool isCaptain)
 	{
 		Name = name;
 		ArtisticName = artisticName;
 		Number = number;
 		Email = email;
+		IsCaptain = isCaptain;
 	}
 
-	public PlayerTempProfile(string name, string artisticName, int number, string email, int teamsId)
+	public PlayerTempProfile(string name, string artisticName, int number, string email,  bool isCaptain, int teamsId)
 	{
 		Name = name;
 		ArtisticName = artisticName;
 		Number = number;
 		Email = email;
 		TeamsId = teamsId;
+		IsCaptain = isCaptain;
 	}
 
 	public PlayerTempProfile() { }
--- Services/PlayerTempProfileService.cs
+++ Services/PlayerTempProfileService.cs
@@ -1,3 +1,4 @@
+using FluentValidation;
 using PlayOffsApi.Models;
 using PlayOffsApi.Validations;
 
@@ -7,21 +8,27 @@ public class PlayerTempProfileService
 {
     private readonly DbService _dbService;
     private readonly ElasticService _elasticService;
+	private readonly TeamService _teamService;
 
 
-    public PlayerTempProfileService(DbService dbService, ElasticService elasticService)
+    public PlayerTempProfileService(DbService dbService, ElasticService elasticService, TeamService teamService)
 	{
 		_dbService = dbService;
         _elasticService = elasticService;
+		_teamService = teamService;
 	}
 
     public async Task<List<string>> CreateValidationAsync(PlayerTempProfile playerTempProfile, Guid userId)
 	{
 		var errorMessages = new List<string>();
+		
+		var team = await _teamService.GetByIdSendAsync(playerTempProfile.TeamsId);
 
 		var playerTempProfileValidator = new PlayerTempProfileValidator();
 
-		var result = playerTempProfileValidator.Validate(playerTempProfile);
+		var result = (team.SportsId == 1) 
+		? playerTempProfileValidator.Validate(playerTempProfile, options => options.IncludeRuleSets("ValidationSoccer"))
+		: playerTempProfileValidator.Validate(playerTempProfile, options => options.IncludeRuleSets("ValidationVolleyBall"));
 
 		if (!result.IsValid)
 		{
@@ -29,7 +36,7 @@ public class PlayerTempProfileService
 			return errorMessages;
 		}
 
-		if(!(await ChecksIfUserIsManager(userId)))
+		if(await ChecksIfUserIsManager(userId))
 		{
 			throw new ApplicationException("Apenas técnicos podem cadastrar jogadores temporários.");
 		}
@@ -44,6 +51,19 @@ public class PlayerTempProfileService
 			throw new ApplicationException("Já exite usuário com o email passado.");
 		}
 
+		if(await ChecksIfNumberAlreadyExistsInPlayerTemp(playerTempProfile.Number))
+		{
+			throw new ApplicationException("Já exite jogador temporário com o número de camisa passado.");
+		}
+
+		if(await ChecksIfTeamAlreadyHasCaptain())
+		{
+			throw new ApplicationException("Time já possui capitão.");
+		}
+
+		//Não pode ter 2 capitães (olhar nos usuários também)
+		//Não pode ter jogador com o mesmo número (olhar nos usuários também)
+
 		await CreateSendAsync(playerTempProfile);
 
 		return errorMessages;
@@ -51,16 +71,13 @@ public class PlayerTempProfileService
 
 	public async Task CreateSendAsync(PlayerTempProfile playerTempProfile)
 	{
-		
 		await _dbService.EditData(
-			"INSERT INTO playertempprofiles (name, artisticname, number, email, teamsid) VALUES (@Name, @ArtisticName, @Number, @Email, @TeamsId)", playerTempProfile);
+			"INSERT INTO playertempprofiles (name, artisticname, number, email, teamsid, iscaptain) VALUES (@Name, @ArtisticName, @Number, @Email, @TeamsId, @IsCaptain)", playerTempProfile);
 	}
 
-	private async Task<bool> ChecksIfUserIsManager(Guid userId) => await _dbService.GetAsync<bool>("SELECT EXISTS(SELECT ManagersId FROM teams WHERE ManagersId = @userId);", new {userId});
-
+	private async Task<bool> ChecksIfUserIsManager(Guid userId) => await _dbService.GetAsync<bool>("SELECT EXISTS(SELECT TeamManagementId FROM users WHERE Id = @userId AND TeamManagementId IS NULL);", new {userId});
 	private async Task<bool> ChecksIfEmailAlreadyExistsInPlayerTempProfiles(string email) => await _dbService.GetAsync<bool>("SELECT EXISTS(SELECT email FROM playertempprofiles WHERE email = @email);", new {email});
 	private async Task<bool> ChecksIfEmailAlreadyExistsInUsers(string email) => await _dbService.GetAsync<bool>("SELECT EXISTS(SELECT emailhash FROM users WHERE emailhash = @email);", new {email});
-
-
-
+	private async Task<bool> ChecksIfNumberAlreadyExistsInPlayerTemp(int number) => await _dbService.GetAsync<bool>("SELECT EXISTS(SELECT name FROM playertempprofiles WHERE number = @number);", new {number});
+	private async Task<bool> ChecksIfTeamAlreadyHasCaptain() => await _dbService.GetAsync<bool>("SELECT EXISTS(SELECT name FROM playertempprofiles WHERE iscaptain = true);", new {});
 }
