4d36a3705738d81f5920ef082bcc2d4f9e72dc52
Caio Ferrari <caio.ferrari@empregare.com.br>
2023-05-27 19:21:18 -0300

    feat: file upload

---
+++ Models/Image.cs
@@ -0,0 +1,11 @@
+﻿namespace PlayOffsApi.Models;
+
+public class Image
+{
+    public Guid FileName { get; init; }
+    public MemoryStream Stream { get; init; }
+    public string Extension { get; init; }
+    public Guid UserId { get; init; }
+    public int OwnerId { get; init; }
+    public string ContentType { get; init; }
+}
