9161ef2f0f1ac6f6fc1fc429c4da636184bca386
Caio Ferrari <caio.ferrari@empregare.com.br>
2023-07-30 16:01:06 -0300

    feat: added picture

--- Models/PlayerTempProfile.cs
+++ Models/PlayerTempProfile.cs
@@ -9,26 +9,29 @@ public class PlayerTempProfile
 	public Guid Id { get; set; }
     public string Name { get; set; }
 	public string ArtisticName { get; set; }
+	public string Picture { get; set; }
     public int Number { get; set; }
     public string Email { get; set; }
 	public int TeamsId { get; set; }
 	public PlayerPosition PlayerPosition { get; set; }
 
-	public PlayerTempProfile(string name, string artisticName, int number, string email)
+	public PlayerTempProfile(string name, string artisticName, int number, string email, string picture)
 	{
 		Name = name;
 		ArtisticName = artisticName;
 		Number = number;
 		Email = email;
+		Picture = picture;
 	}
 
-	public PlayerTempProfile(string name, string artisticName, int number, string email, int teamsId)
+	public PlayerTempProfile(string name, string artisticName, int number, string email, int teamsId, string picture)
 	{
 		Name = name;
 		ArtisticName = artisticName;
 		Number = number;
 		Email = email;
 		TeamsId = teamsId;
+		Picture = picture;
 	}
 
 	public PlayerTempProfile() { }
