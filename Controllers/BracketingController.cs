13cbe04ea00149f9335de9c461bcf0d94be49906
AlexsandroCampos <alexsandro.campos@aluno.ifsp.edu.br>
2023-08-05 17:16:07 -0300

    feat: create championship knockout stage with group stage

--- Controllers/BracketingController.cs
+++ Controllers/BracketingController.cs
@@ -73,7 +73,7 @@ public class BracketingController : ApiBaseController
         var result = new List<Match>();
         try
         {
-            result = await _bracketingService.CreateSimpleKnockoutGroupStage(championshipId);
+            result = await _bracketingService.CreateGroupStage(championshipId);
             return ApiOk(result);
         }
         catch (ApplicationException ex)
