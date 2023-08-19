a823a05a447f804eb1a0861d37887d2527f6ee15
AlexsandroCampos <alexsandro.campos@aluno.ifsp.edu.br>
2023-08-19 16:43:18 -0300

    feat: order players by position id

--- Controllers/TeamController.cs
+++ Controllers/TeamController.cs
@@ -217,6 +217,7 @@ public class TeamController : ApiBaseController
         try
         {
             var players = await _teamService.GetPlayersOfTeamValidation(id); 
+            players = players.OrderBy(u => u.PlayerPosition).ToList();
             return ApiOk(players.Select(m => new { id = m.Id, name = m.Name, artisticName = m.ArtisticName, number = m.Number, teamsId = m.PlayerTeamId, playerPosition = m.PlayerPosition, isCaptain = m.IsCaptain, picture = m.Picture }));
         }
         catch (ApplicationException ex)
