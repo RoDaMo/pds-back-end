b64709b9598271e6f077fb0751a288040bd7d5c4
AlexsandroCampos <alexsandro.campos@aluno.ifsp.edu.br>
2023-09-03 13:05:35 -0300

    fix: fixing the query to verify if user exists

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -114,7 +114,7 @@ public class AuthService
 		var result2 = await userValidator.ValidateAsync(user, options => options.IncludeRuleSets("IdentificadorEmail"));
 
 		if (result.IsValid || result2.IsValid)
-			return await _dbService.GetAsync<bool>("SELECT COUNT(1) FROM users WHERE username = @Username OR email = @Email", user);
+			return await _dbService.GetAsync<bool>("SELECT COUNT(1) FROM users WHERE (username = @Username OR email = @Email) AND Deleted = false ", user);
 
 		throw new ApplicationException(Resource.InvalidUsername);
 	}
