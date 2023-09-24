a2b6a9d346239b91927ce549398977b5b6a9a32e
Caio Ferrari <caio.ferrari@empregare.com.br>
2023-09-23 23:53:32 -0300

    feat: report system

---
+++ Controllers/ReportController.cs
@@ -0,0 +1,259 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PlayOffsApi.API;
+using PlayOffsApi.Enum;
+using PlayOffsApi.Models;
+using PlayOffsApi.Services;
+
+namespace PlayOffsApi.Controllers;
+
+/// <summary>
+/// Endpoint dedicado à envio de denúncias de usuários, assim como o gerenciamento das mesmas.
+/// </summary>
+[ApiController]
+[Authorize]
+[Route("/reports")]
+public class ReportController : ApiBaseController
+{
+    private readonly ReportService _reportService;
+    private readonly ErrorLogService _error;
+    public ReportController(ReportService reportService, ErrorLogService error)
+    {
+        _reportService = reportService;
+        _error = error;
+    }
+    
+    /// <summary>
+    /// Criar uma denúncia
+    /// </summary>
+    /// <remarks>
+    /// Exemplo de requisição:
+    /// 
+    ///		POST /reports
+    ///		{
+    ///        "AuthorId": "e0b595dc-bcc3-4d08-ab3b-68b194a45974",
+    ///        "ReportedType": 0,
+    ///        "ReportedUserId": "e0b595dc-bcc3-4d08-ab3b-68b194a45974",
+    ///        "ReportedTeamId": 0,
+    ///        "ReportedChampionshipId": 5,
+    ///        "Description": "Teste"
+    ///     }
+    ///		
+    /// </remarks>
+    /// <param name="report"></param>
+    /// <response code="200">Retorna se o usuário existe ou não.</response>
+    /// <response code="401">Retorna uma falha indicando algum erro cometido na requisição.</response>
+    /// <returns>
+    ///	Exemplo de retorno:
+    ///
+    ///		{
+    ///			"message": "",
+    ///			"succeed": true,
+    ///			"results": true
+    ///		}
+    ///		
+    /// </returns>
+    [HttpPost]
+    public async Task<IActionResult> CreateReport(Report report)
+    {
+        try
+        {
+            var userId =  Guid.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value!);
+            report.AuthorId = userId;
+            await _reportService.CreateReportValidation(report);
+            return ApiOk();
+        }
+        catch (ApplicationException ex)
+        {
+            await _error.HandleExceptionValidationAsync(HttpContext, ex);
+            return ApiBadRequest(ex.Message);
+        }
+    }
+
+    /// <summary>
+    /// Obter por tipo
+    /// </summary>
+    /// <param name="type"></param>
+    /// <param name="completed"></param>
+    /// <remarks>
+    /// Exemplo de requisição:
+    ///
+    ///     GET /reports?type=0&amp;completed=false
+    /// 
+    /// </remarks>
+    /// <returns>\
+    /// Exemplo de retorno:
+    /// 
+    ///     {
+    ///         "message": "",
+    ///         "succeed": true,
+    ///         "results": [
+    ///         {
+    ///             "id": 3,
+    ///             "authorId": "140804b5-e3b5-495e-92e8-8f6c534972ee",
+    ///             "completed": false,
+    ///             "reportType": 0,
+    ///             "reportedUserId": null,
+    ///             "reportedTeamId": null,
+    ///             "reportedChampionshipId": 5,
+    ///             "description": "Teste"
+    ///         }
+    ///         ]
+    ///     }
+    /// </returns>
+    [HttpGet]
+    [Authorize(Roles = "admin")]
+    public async Task<IActionResult> GetReportsByType(ReportType type, bool completed)
+    {
+        try
+        {
+            return ApiOk(await _reportService.GetAllByTypeValidation(type, completed));
+        }
+        catch (ApplicationException ex)
+        {
+            await _error.HandleExceptionValidationAsync(HttpContext, ex);
+            return ApiBadRequest(ex.Message);
+        }
+    }
+
+    /// <summary>
+    /// Obter por ID
+    /// </summary>
+    /// <remarks>
+    /// Exemplo de requisição:
+    /// 
+    ///		GET /reports/3
+    /// 
+    /// </remarks>
+    /// <param name="id"></param>
+    /// <returns>
+    /// Exemplo de retorno:
+    /// 
+    ///     {
+    ///         "message": "",
+    ///         "succeed": true,
+    ///         "results": {
+    ///             "id": 3,
+    ///             "authorId": "140804b5-e3b5-495e-92e8-8f6c534972ee",
+    ///             "completed": false,
+    ///             "reportType": 0,
+    ///             "reportedUserId": null,
+    ///             "reportedTeamId": null,
+    ///             "reportedChampionshipId": 5,
+    ///             "description": "Teste"
+    ///         }
+    ///     }
+    /// </returns>
+    [HttpGet]
+    [Authorize(Roles = "admin")]
+    [Route("/reports/{id:int}")]
+    public async Task<IActionResult> GetReportById(int id)
+    {
+        try
+        {
+            return ApiOk(await _reportService.GetByIdValidation(id));
+        }
+        catch (ApplicationException ex)
+        {
+            await _error.HandleExceptionValidationAsync(HttpContext, ex);
+            return ApiBadRequest(ex.Message);
+        }
+    }
+    
+    /// <summary>
+    /// Obter por ID de usuário
+    /// </summary>
+    /// <param name="id"></param>
+    /// <remarks>
+    /// Exemplo de requisição:
+    ///
+    ///     GET /reports/user/140804b5-e3b5-495e-92e8-8f6c534972ee
+    /// 
+    /// </remarks>
+    /// <returns>\
+    /// Exemplo de retorno:
+    /// 
+    ///     {
+    ///         "message": "",
+    ///         "succeed": true,
+    ///         "results": [
+    ///         {
+    ///             "id": 3,
+    ///             "authorId": "140804b5-e3b5-495e-92e8-8f6c534972ee",
+    ///             "completed": false,
+    ///             "reportType": 0,
+    ///             "reportedUserId": null,
+    ///             "reportedTeamId": null,
+    ///             "reportedChampionshipId": 5,
+    ///             "description": "Teste"
+    ///         }
+    ///         ]
+    ///     }
+    /// </returns>
+    [HttpGet]
+    [Authorize(Roles = "admin")]
+    [Route("/reports/user/{id:guid}")]
+    public async Task<IActionResult> GetReportByUserId(Guid id)
+    {
+        try
+        {
+            return ApiOk(await _reportService.GetReportsFromUserValidation(id));
+        }
+        catch (ApplicationException ex)
+        {
+            await _error.HandleExceptionValidationAsync(HttpContext, ex);
+            return ApiBadRequest(ex.Message);
+        }
+    }
+    
+    /// <summary>
+    /// Atualizar status da denúncia
+    /// </summary>
+    /// <param name="report"></param>
+    /// <remarks>
+    /// Exemplo de requisição:
+    ///
+    ///     PUT /reports
+    ///     {
+    ///         "Id": 3,
+    ///         "Completed": true
+    ///     }
+    /// 
+    /// </remarks>
+    /// <returns>\
+    /// Exemplo de retorno:
+    /// 
+    ///     {
+    ///         "message": "",
+    ///         "succeed": true,
+    ///         "results": [
+    ///         {
+    ///             "id": 3,
+    ///             "authorId": "140804b5-e3b5-495e-92e8-8f6c534972ee",
+    ///             "completed": false,
+    ///             "reportType": 0,
+    ///             "reportedUserId": null,
+    ///             "reportedTeamId": null,
+    ///             "reportedChampionshipId": 5,
+    ///             "description": "Teste"
+    ///         }
+    ///         ]
+    ///     }
+    /// </returns>
+    [HttpPut]
+    [Authorize(Roles = "admin")]
+    public async Task<IActionResult> UpdateStatusReport(Report report)
+    {
+        try
+        {
+            await _reportService.SetReportAsCompletedValidation(report.Id, report.Completed);
+            return ApiOk();
+        }
+        catch (ApplicationException ex)
+        {
+            await _error.HandleExceptionValidationAsync(HttpContext, ex);
+            return ApiBadRequest(ex.Message);
+        }
+    }
+}
---
+++ Models/Report.cs
@@ -0,0 +1,15 @@
+﻿using PlayOffsApi.Enum;
+
+namespace PlayOffsApi.Models;
+
+public class Report
+{
+    public int Id { get; set; }
+    public Guid AuthorId { get; set; }
+    public bool Completed { get; set; }
+    public ReportType ReportType { get; set; }
+    public Guid? ReportedUserId { get; set; }
+    public int? ReportedTeamId { get; set; }
+    public int? ReportedChampionshipId { get; set; }
+    public string Description { get; set; }
+}
